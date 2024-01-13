dbebf5ab54b961d95545044722c8b963dbf7d307
aliicayli <aliicayli@gmail.com>
2024-01-13 03:52:54 +0300

    Added booking complete page

---
+++ app/src/main/java/com/msku/example/BookingCompleteActivity.java
@@ -0,0 +1,16 @@
+package com.msku.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.msku.example.rentcar.R;
+
+public class BookingCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_complete_activity);
+    }
+}
--- app/src/main/java/com/msku/example/InformationAdActivity.java
+++ app/src/main/java/com/msku/example/InformationAdActivity.java
@@ -2,15 +2,24 @@ package com.msku.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.msku.example.rentcar.R;
 
 public class InformationAdActivity extends AppCompatActivity {
-
+Button continueBookingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_ad);
+        continueBookingButton = findViewById(R.id.continueBooking);
+        ContinueBookingButtonClick();
     }
-}
+
+    private void ContinueBookingButtonClick() {
+        continueBookingButton.setOnClickListener(view ->{
+            startActivity(new Intent(this,BookingCompleteActivity.class));
+        });
+    }}
