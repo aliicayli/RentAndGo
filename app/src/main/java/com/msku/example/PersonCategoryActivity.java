aefc9895124eb0d18632c27b2fa4e2177ef08e7d
aliicayli <aliicayli@gmail.com>
2024-01-17 02:20:49 +0300

    Added firebase authantication

--- app/src/main/java/com/msku/example/PersonCategoryActivity.java
+++ app/src/main/java/com/msku/example/PersonCategoryActivity.java
@@ -6,20 +6,31 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.msku.example.rentcar.R;
 
 public class PersonCategoryActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseUser user;
 CardView carOwnerCardView;
 CardView passengerCardView;
+Button logOutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_category);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
         carOwnerCardView = findViewById(R.id.carOwner);
         passengerCardView = findViewById(R.id.passenger);
+        logOutButton = findViewById(R.id.logOutButton);
         CarOwnerCardViewClick();
         PassengerCardViewClick();
+        LogOutButtonClick();
     }
     private void CarOwnerCardViewClick() {
         carOwnerCardView.setOnClickListener(view ->{
@@ -30,6 +41,15 @@ CardView passengerCardView;
     private void PassengerCardViewClick() {
         passengerCardView.setOnClickListener(view ->{
             startActivity(new Intent(this,CarCategoryActivity.class));
+        });
+    }
+
+    private void LogOutButtonClick() {
+        logOutButton.setOnClickListener(view ->{
+        FirebaseAuth.getInstance().signOut();
+            Intent intent =new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
