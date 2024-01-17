db3f67f0b55bd9cf871ff05a742af8e23166d9cb
aliicayli <aliicayli@gmail.com>
2024-01-17 07:36:08 +0300

    Update for backup
    This reverts commit 523d8bdabd5c6b8be8cdac229e76125d3e99cefe.

--- app/src/main/java/com/msku/example/LoginActivity.java
+++ app/src/main/java/com/msku/example/LoginActivity.java
@@ -1,41 +1,21 @@
 package com.msku.example;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.msku.example.rentcar.R;
 
 public class LoginActivity extends AppCompatActivity {
-    FirebaseAuth auth;
     TextView registerTextView;
     TextView emailTextView;
     TextView passwordTextView;
     Button loginButton;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = auth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent =new Intent(getApplicationContext(),PersonCategoryActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +25,6 @@ public class LoginActivity extends AppCompatActivity {
         emailTextView = findViewById(R.id.email);
         passwordTextView = findViewById(R.id.password);
         loginButton = findViewById(R.id.login);
-        auth = FirebaseAuth.getInstance();
         RegisterTextViewClick();
         LoginButtonClick();
     }
@@ -57,42 +36,13 @@ public class LoginActivity extends AppCompatActivity {
 
     private void LoginButtonClick() {
         loginButton.setOnClickListener(view ->{
-            String email = "";
-            String password = "";
-
-            email = String.valueOf(emailTextView.getText());
-            password = String.valueOf(passwordTextView.getText());
-
-            if (TextUtils.isEmpty(email)) {
-                Toast.makeText(LoginActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
-                return;
+            String emailText = emailTextView.getText().toString();
+            String passwordText = passwordTextView.getText().toString();
+            String loginQuestText = UserManagement.users.get(emailText).password;
+            if (loginQuestText.equals(passwordText)) {
+                UserManagement.loggedEmail = emailText;
+                startActivity(new Intent(this,PersonCategoryActivity.class));
             }
-            if (TextUtils.isEmpty(password)) {
-                Toast.makeText(LoginActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "Login Successful",Toast.LENGTH_SHORT).show();
-                                Intent intent =new Intent(getApplicationContext(),PersonCategoryActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
-                            else {
-                                Toast.makeText(LoginActivity.this, "Login failed.",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-
-
-            //if (loginQuestText.equals(password)) {
-                //UserManagement.loggedEmail = email;
-                //startActivity(new Intent(this,PersonCategoryActivity.class));
-           //}
         });
     }
 
--- app/src/main/java/com/msku/example/RegisterActivity.java
+++ app/src/main/java/com/msku/example/RegisterActivity.java
@@ -8,19 +8,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Debug;
-import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.Firebase;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.msku.example.rentcar.R;
 
 import java.util.ArrayList;
@@ -29,11 +21,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-import kotlin.io.TextStreamsKt;
-
 public class RegisterActivity extends AppCompatActivity {
-    FirebaseAuth auth;
-    FirebaseUser user;
     EditText fullNameEditText;
     EditText emailEditText;
     EditText dateOfBirthEditText;
@@ -52,8 +40,6 @@ public class RegisterActivity extends AppCompatActivity {
         phoneNumberEditText = findViewById(R.id.phoneNumber);
         cityEditText = findViewById(R.id.city);
         passwordEditText = findViewById(R.id.password);
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
         RegisterButtonClick();
     }
 
@@ -65,59 +51,35 @@ public class RegisterActivity extends AppCompatActivity {
             String phoneNumber = "";
             String city = "";
             String password = "";
-            fullName = String.valueOf(fullNameEditText.getText());
-            email = String.valueOf(emailEditText.getText());
-            dateOfBirth = String.valueOf( dateOfBirthEditText.getText());
-            phoneNumber = String.valueOf( phoneNumberEditText.getText());
-            city = String.valueOf( cityEditText.getText());
-            password = String.valueOf( passwordEditText.getText());
-            if (TextUtils.isEmpty(fullName)) {
-                Toast.makeText(RegisterActivity.this,"Enter Name",Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if (TextUtils.isEmpty(email)) {
-                Toast.makeText(RegisterActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if (TextUtils.isEmpty(dateOfBirth)) {
-                Toast.makeText(RegisterActivity.this,"Enter Date of Birth",Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if (TextUtils.isEmpty(phoneNumber)) {
-                Toast.makeText(RegisterActivity.this,"Enter Phone Number",Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if (TextUtils.isEmpty(city)) {
-                Toast.makeText(RegisterActivity.this,"Enter City",Toast.LENGTH_SHORT).show();
-                return;
-            }
-             if (TextUtils.isEmpty(password)) {
-                Toast.makeText(RegisterActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
-                return;
-            }
+            if (fullNameEditText.getText() == null) fullName = "";
+            else fullName = fullNameEditText.getText().toString();
 
-            auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(RegisterActivity.this,"Account created.",Toast.LENGTH_SHORT).show();
-                                Intent intent =new Intent(getApplicationContext(),LoginActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
-                            else {
-                                Toast.makeText(RegisterActivity.this,"Authentication failed.",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+            if (emailEditText.getText() == null) email = "";
+            else email = emailEditText.getText().toString();
 
+            if (dateOfBirthEditText.getText() == null) dateOfBirth = "";
+            else dateOfBirth = dateOfBirthEditText.getText().toString();
 
+            if (phoneNumberEditText.getText() == null) phoneNumber = "";
+            else phoneNumber = phoneNumberEditText.getText().toString();
 
-            //User user = new User(fullName,email,dateOfBirth,phoneNumber,city,password);
-            //UserManagement.users.put(user.email,user);
+            if (cityEditText.getText() == null) city = "";
+            else city = cityEditText.getText().toString();
 
-            //startActivity(new Intent(this,LoginActivity.class));
+            if (passwordEditText.getText() == null) password = "";
+            else password = passwordEditText.getText().toString();
+
+            User user = new User(fullName,email,dateOfBirth,phoneNumber,city,password);
+            //ArrayList<String> data = new ArrayList<>() ;
+            //data.add(user.fullName);
+            //data.add(user.email);
+            //data.add(user.dateOfBirth);
+            //data.add(user.phoneNumber);
+            //data.add(user.city);
+            //data.add(user.password);
+            UserManagement.users.put(user.email,user);
+
+            startActivity(new Intent(this,LoginActivity.class));
         });
     }
 }
